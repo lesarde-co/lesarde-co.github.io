6ff3efaa32f5b8d291ea60ae2f6b286a32e16a02
Tom Dowdell <tom@lesarde.com>
2019-04-23 17:07:03 -0700

    Version 0.3.0

---
+++ mono-config.js
@@ -0,0 +1,6 @@
+config = {
+ 	vfs_prefix: "managed",
+ 	deploy_prefix: "managed",
+ 	enable_debugging: 0,
+ 	file_list: [ "Demo.dll","mscorlib.dll","Lesarde.Frogui.Api.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Numerics.dll","System.Core.dll","Newtonsoft.Json.dll","netstandard.dll","System.Data.dll","System.Transactions.dll","System.Drawing.Common.dll","System.IO.Compression.dll","System.IO.Compression.FileSystem.dll","System.ComponentModel.Composition.dll","System.Net.Http.dll","System.Runtime.Serialization.dll","System.ServiceModel.Internals.dll","System.Web.Services.dll","System.Xml.Linq.dll","Microsoft.Extensions.Logging.Abstractions.dll","System.Reactive.dll","System.Runtime.InteropServices.WindowsRuntime.dll","Lesarde.Base.dll","Microsoft.Extensions.Logging.dll","Microsoft.Extensions.DependencyInjection.Abstractions.dll","Microsoft.Extensions.Options.dll","Microsoft.Extensions.Primitives.dll","System.Memory.dll","System.Runtime.CompilerServices.Unsafe.dll","Lesarde.Frogui.Scope.dll","Microsoft.Extensions.Logging.Debug.dll","WebAssembly.Bindings.dll","WebAssembly.Net.Http.dll","WebAssembly.Net.WebSockets.dll" ],
+}
